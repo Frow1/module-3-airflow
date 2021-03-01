4cd3da6d7412d7c18e112c2f74918e8894e3c5ae
Frow1 <72742598+Frow1@users.noreply.github.com>
2021-03-01 16:55:44 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -16,17 +16,20 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r falconheavy",
-    dag=dag
-)
+for rocket in ('falcon1', 'falcon9', 'falconheavy', 'all'):
+    params = {"rocket": rocket}
+    t1 = BashOperator(
+        task_id="get_data" + rocket, 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data".format(" -r {{ params.rocket }}" if rocket !='all' else ""),
+        params=params
+        dag=dag
+    )
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "falconheavy"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
+    t2 = BashOperator(
+        task_id="print_data" + rocket, 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+        params=params
+        dag=dag
+    )
 
 t1 >> t2
